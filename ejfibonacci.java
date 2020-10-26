2fe558f856d2663b95673018c8b909998b78e9f2
JorgeGallego00 <72868874+JorgeGallego00@users.noreply.github.com>
2020-10-26 22:56:07 +0100

    Add files via upload

---
+++ ejfibonacci.java
@@ -0,0 +1,33 @@
+package ejersjava;
+import java.util.Scanner;
+public class ejfibonacci {
+
+	
+		
+		private static int fibonaci(int num) {
+			
+		    int suma = 1;
+	     	int valor1 = 0;
+	    	int valor2 = 1;
+
+			for(int i = 1;i < num; i++) {
+				
+				suma = valor1 + valor2;
+				valor1 = valor2;
+				valor2 = suma;
+			}	
+			return suma;	
+		}
+		
+		public static void main(String[]args) {
+			Scanner sc = new Scanner(System.in);
+		    System.out.println("Escribe una posicion");
+		    int posicion = Integer.parseInt(sc.nextLine());
+		   	int valor = fibonaci(posicion);
+			System.out.println("El numero que hay en la posicion" + posicion + "en la sucesion de fibonacci es  :" + valor );
+		    
+		}
+		
+		
+	}
+
